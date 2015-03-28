3c7e96220bc9943bea8685595db4072204d237c6
Dyonovan <mcbetacraft@gmail.com>
2015-03-28 00:54:13 -0400

    New Slot work

--- src/main/java/com/dyonovan/neotech/common/container/SlotMB.java
+++ src/main/java/com/dyonovan/neotech/common/container/SlotMB.java
@@ -1,4 +1,46 @@
 package com.dyonovan.neotech.common.container;
 
-public class SlotMB {
+import com.dyonovan.neotech.handlers.ItemHandler;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class SlotMB extends Slot {
+
+    public SlotMB(IInventory inventoryIn, int index, int xPosition, int yPosition) {
+        super(inventoryIn, index, xPosition, yPosition);
+    }
+
+    @Override
+    public void putStack(ItemStack stack) {
+        if (stack == null) return;
+        if (stack.getItem() != ItemHandler.upgradeMB) return;
+        if (!stack.hasTagCompound()) return;
+        for (int i = 0; i < 4; i++) {
+            if (inventory.getStackInSlot(i) != null) return;
+        }
+        NBTTagCompound tag = stack.getTagCompound();
+        int count = 0;
+        if (tag.hasKey("Speed")) {
+            inventory.setInventorySlotContents(count, new ItemStack(ItemHandler.speedProcessor, tag.getInteger("Speed")));
+            ++count;
+        }
+        if (tag.hasKey("Efficiency")) {
+            inventory.setInventorySlotContents(count, new ItemStack(ItemHandler.effFan, tag.getInteger("Efficiency")));
+            ++count;
+        }
+        if (tag.hasKey("Capacity")) {
+            inventory.setInventorySlotContents(count, new ItemStack(ItemHandler.capRam, tag.getInteger("Capacity")));
+            ++count;
+        }
+        if (tag.hasKey("AutoOutput")) {
+            inventory.setInventorySlotContents(count, new ItemStack(ItemHandler.ioPort, 1));
+            ++count;
+        }
+
+        super.putStack(stack);
+    }
+
+
 }
