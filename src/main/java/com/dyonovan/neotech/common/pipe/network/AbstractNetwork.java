f3e0531c5e0174b2f8fca3c6f4bcf86d10e2349a
Paul Davis <pauldavis101@gmail.com>
2015-04-02 18:26:27 -0500

    Network start

---
+++ src/main/java/com/dyonovan/neotech/common/pipe/network/AbstractNetwork.java
@@ -0,0 +1,8 @@
+package com.dyonovan.neotech.common.pipe.network;
+
+import java.util.List;
+
+public class AbstractNetwork<S, P> {
+    protected List<S> sources;
+    protected List<P> providers;
+}
