961cc9b7872457979526326c4aa682500a6205d4
Paul Davis <pauldavis101@gmail.com>
2016-02-07 22:37:23 -0600

    Start of pump

--- src/main/scala/com/dyonovan/neotech/common/tiles/machines/operators/TilePump.scala
+++ src/main/scala/com/dyonovan/neotech/common/tiles/machines/operators/TilePump.scala
@@ -1,8 +1,11 @@
 package com.dyonovan.neotech.common.tiles.machines.operators
 
 import com.teambr.bookshelf.common.tiles.traits.{FluidHandler, UpdatingTile}
+import net.minecraft.block.BlockLiquid
+import net.minecraft.init.Blocks
 import net.minecraft.nbt.NBTTagCompound
-import net.minecraftforge.fluids.FluidTank
+import net.minecraft.util.EnumFacing
+import net.minecraftforge.fluids._
 
 /**
   * This file was created for NeoTech
@@ -17,12 +20,56 @@ import net.minecraftforge.fluids.FluidTank
 class TilePump extends UpdatingTile with FluidHandler {
 
     val RANGE = 50
+    val TANK = 0
 
     override def setupTanks(): Unit = {
         tanks += new FluidTank(bucketsToMB(10))
     }
 
     override def onTankChanged(tank: FluidTank): Unit = worldObj.markBlockForUpdate(pos)
+
+    override def onServerTick() : Unit = {
+        if(tanks(TANK).getFluidAmount < tanks(TANK).getCapacity) {
+            worldObj.getBlockState(pos.offset(EnumFacing.DOWN)).getBlock match {
+                case fluid: BlockFluidBase =>
+                    val level = fluid.getFilledPercentage(worldObj, pos.offset(EnumFacing.DOWN))
+                    if (level == 1.0F) {
+                        val fluidStack = new FluidStack(fluid.getFluid, 1000)
+                        if (fill(EnumFacing.DOWN, fluidStack, doFill = false) >= 1000) {
+                            fill(EnumFacing.DOWN, fluidStack, doFill = true)
+                            worldObj.setBlockToAir(pos.offset(EnumFacing.DOWN))
+                        }
+                    }
+                case water: Blocks.water.type if worldObj.getBlockState(pos.offset(EnumFacing.DOWN)).getValue(BlockLiquid.LEVEL).intValue == 0 =>
+                    val fluidStack = new FluidStack(FluidRegistry.WATER, 1000)
+                    if (fill(EnumFacing.DOWN, fluidStack, doFill = false) >= 1000) {
+                        fill(EnumFacing.DOWN, fluidStack, doFill = true)
+                        worldObj.setBlockToAir(pos.offset(EnumFacing.DOWN))
+                    }
+                case lava: Blocks.lava.type if worldObj.getBlockState(pos.offset(EnumFacing.DOWN)).getValue(BlockLiquid.LEVEL).intValue == 0 =>
+                    val fluidStack = new FluidStack(FluidRegistry.LAVA, 1000)
+                    if (fill(EnumFacing.DOWN, fluidStack, doFill = false) >= 1000) {
+                        fill(EnumFacing.DOWN, fluidStack, doFill = true)
+                        worldObj.setBlockToAir(pos.offset(EnumFacing.DOWN))
+                    }
+                case _ =>
+            }
+        }
+        tryOutput()
+    }
+
+    def tryOutput() : Unit = {
+        for(dir <- EnumFacing.values) {
+            worldObj.getTileEntity(pos.offset(dir)) match {
+                case otherTank : IFluidHandler =>
+                    if(tanks(TANK).getFluid != null && tanks(TANK).getFluid.getFluid != null && otherTank.canFill(dir.getOpposite, tanks(TANK).getFluid.getFluid)
+                            && otherTank.fill(dir.getOpposite, drain(dir, 1000, doDrain = false), false) > 0)
+                        otherTank.fill(dir.getOpposite, drain(dir, 1000, doDrain = true), true)
+                case _ =>
+            }
+        }
+
+    }
 
     override def writeToNBT(tag : NBTTagCompound) : Unit = {
         super[TileEntity].writeToNBT(tag)
