6d2ada4e9f00845dabb3f4644417b0d3784337a9
Paul Davis <pauldavis101@gmail.com>
2016-02-09 16:22:12 -0600

    Lazy fix is lazy

--- src/main/scala/com/dyonovan/neotech/client/renderers/TileMachineIORenderer.scala
+++ src/main/scala/com/dyonovan/neotech/client/renderers/TileMachineIORenderer.scala
@@ -17,19 +17,24 @@ import net.minecraft.util.EnumFacing
   */
 class TileMachineIORenderer extends TileRenderHelper[AbstractMachine] {
     override def renderTileEntityAt(te: AbstractMachine, x: Double, y: Double, z: Double, partialTicks: Float, destroyStage: Int): Unit = {
-        if (te != null && te.shouldRenderInputOutputOnTile && te.getWorld.getBlockState(te.getPos) != null) {
-            val facing = te.getWorld.getBlockState(te.getPos).getValue(PropertyRotation.FOUR_WAY)
-            for (dir <- EnumFacing.values()) {
-                if (te.canInputFromSide(dir, facing) && te.canOutputFromSide(dir, facing))
-                    renderIconOnBlock(Minecraft.getMinecraft.getTextureMapBlocks.getAtlasSprite("neotech:blocks/inputOutputFace"),
-                        0, te.getDirFromFacing(dir, facing), te.getDirFromFacing(dir, facing), new LocationDouble(x, y, z), 16.0F, 0.0, 0.0, -0.001)
-                else if (!te.canInputFromSide(dir, facing) && te.canOutputFromSide(dir, facing))
-                    renderIconOnBlock(Minecraft.getMinecraft.getTextureMapBlocks.getAtlasSprite("neotech:blocks/outputFace"),
-                        0, te.getDirFromFacing(dir, facing), te.getDirFromFacing(dir, facing), new LocationDouble(x, y, z), 16.0F, 0.0F, 0.0F, -0.001)
-                else if (te.canInputFromSide(dir, facing) && !te.canOutputFromSide(dir, facing))
-                    renderIconOnBlock(Minecraft.getMinecraft.getTextureMapBlocks.getAtlasSprite("neotech:blocks/inputFace"),
-                        0, te.getDirFromFacing(dir, facing), te.getDirFromFacing(dir, facing), new LocationDouble(x, y, z), 16.0F, 0.0, 0.0, -0.001)
+        try {
+            if (te != null && te.shouldRenderInputOutputOnTile && te.getWorld.getBlockState(te.getPos) != null) {
+                val facing = te.getWorld.getBlockState(te.getPos).getValue(PropertyRotation.FOUR_WAY)
+                for (dir <- EnumFacing.values()) {
+                    if (te.canInputFromSide(dir, facing) && te.canOutputFromSide(dir, facing))
+                        renderIconOnBlock(Minecraft.getMinecraft.getTextureMapBlocks.getAtlasSprite("neotech:blocks/inputOutputFace"),
+                            0, te.getDirFromFacing(dir, facing), te.getDirFromFacing(dir, facing), new LocationDouble(x, y, z), 16.0F, 0.0, 0.0, -0.001)
+                    else if (!te.canInputFromSide(dir, facing) && te.canOutputFromSide(dir, facing))
+                        renderIconOnBlock(Minecraft.getMinecraft.getTextureMapBlocks.getAtlasSprite("neotech:blocks/outputFace"),
+                            0, te.getDirFromFacing(dir, facing), te.getDirFromFacing(dir, facing), new LocationDouble(x, y, z), 16.0F, 0.0F, 0.0F, -0.001)
+                    else if (te.canInputFromSide(dir, facing) && !te.canOutputFromSide(dir, facing))
+                        renderIconOnBlock(Minecraft.getMinecraft.getTextureMapBlocks.getAtlasSprite("neotech:blocks/inputFace"),
+                            0, te.getDirFromFacing(dir, facing), te.getDirFromFacing(dir, facing), new LocationDouble(x, y, z), 16.0F, 0.0, 0.0, -0.001)
+                }
             }
+        } catch {
+            case e : IllegalArgumentException => //Weird Minecraft stuff
+            case _: Throwable =>
         }
     }
 }
