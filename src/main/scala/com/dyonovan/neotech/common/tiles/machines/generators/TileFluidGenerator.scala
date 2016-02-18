0ec0ec7a4010c1f4dbfb8ed124f85a4bc3e73b16
Paul Davis <pauldavis101@gmail.com>
2016-02-18 12:08:43 -0600

    Safety check for non standard fluid handlers
    closes #136 probably

--- src/main/scala/com/dyonovan/neotech/common/tiles/machines/generators/TileFluidGenerator.scala
+++ src/main/scala/com/dyonovan/neotech/common/tiles/machines/generators/TileFluidGenerator.scala
@@ -148,7 +148,8 @@ class TileFluidGenerator extends MachineGenerator with IFluidHandler {
             if(canInputFromSide(dir)) {
                 worldObj.getTileEntity(pos.offset(dir)) match {
                     case otherTank : IFluidHandler =>
-                        if(otherTank.getTankInfo(dir.getOpposite)(0).fluid != null && canFill(dir, otherTank.getTankInfo(dir.getOpposite)(0).fluid.getFluid))
+                        if(otherTank.getTankInfo(dir.getOpposite) != null && otherTank.getTankInfo(dir.getOpposite).nonEmpty &&
+                                otherTank.getTankInfo(dir.getOpposite)(0).fluid != null && canFill(dir, otherTank.getTankInfo(dir.getOpposite)(0).fluid.getFluid))
                             fill(dir, otherTank.drain(dir.getOpposite, 1000, true), doFill = true)
                     case _ =>
                 }
