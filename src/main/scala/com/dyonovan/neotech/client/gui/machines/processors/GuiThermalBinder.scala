e6a1fcc42958977a977580962647e690a0813aa5
Paul Davis <pauldavis101@gmail.com>
2016-02-25 20:36:19 -0600

    Weird, fake the client out

--- src/main/scala/com/dyonovan/neotech/client/gui/machines/processors/GuiThermalBinder.scala
+++ src/main/scala/com/dyonovan/neotech/client/gui/machines/processors/GuiThermalBinder.scala
@@ -65,7 +65,7 @@ class GuiThermalBinder (player: EntityPlayer, tileEntity: TileThermalBinder) ext
 
         components += new GuiComponentButton(120, 75, 40, 20, "neotech.text.start") {
             override def doAction(): Unit = {
-                if(Minecraft.getMinecraft.thePlayer.capabilities.isCreativeMode) tileEntity.isRunning = true
+                tileEntity.isRunning = true
                 if (tileEntity.getStackInSlot(tileEntity.OBJECT_INPUT) != null && tileEntity.hasValidInput) {
                     if (!Minecraft.getMinecraft.thePlayer.capabilities.isCreativeMode)
                         tileEntity.sendValueToServer(tileEntity.RUNNING_VARIABLE_ID, 0)
