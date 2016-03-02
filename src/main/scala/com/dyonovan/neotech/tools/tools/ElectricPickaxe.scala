93d22dabe1e2d6b1f907da310ec507aa875b195a
Paul Davis <pauldavis101@gmail.com>
2016-03-02 16:06:40 -0600

    Checks if active before placing

--- src/main/scala/com/dyonovan/neotech/tools/tools/ElectricPickaxe.scala
+++ src/main/scala/com/dyonovan/neotech/tools/tools/ElectricPickaxe.scala
@@ -172,6 +172,7 @@ class ElectricPickaxe extends ItemPickaxe(ToolHelper.NEOTECH) with BaseElectricT
         if(ModifierLighting.hasLighting(stack) && entityIn.isInstanceOf[EntityPlayer]) {
             val pos = new BlockPos(entityIn.posX.toInt, entityIn.posY.toInt, entityIn.posZ.toInt)
             if(worldIn.getLightBrightness(pos) < 0.5 &&
+                    ModifierLighting.isLightingActive(stack) &&
                     worldIn.getBlockState(pos).getBlock.isAir(worldIn, pos) &&
                     worldIn.getBlockState(pos).getBlock != BlockManager.lightSource) {
                 worldIn.setBlockState(pos, BlockManager.lightSource.getDefaultState)
