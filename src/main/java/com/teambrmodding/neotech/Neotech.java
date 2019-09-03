4838a77a280c7443ac083bf105c95119c9debabc
Dyonovan <dyonovan@gmail.com>
2019-09-02 23:08:04 -0400

    Initial Setup

--- src/main/java/com/teambrmodding/neotech/Neotech.java
+++ src/main/java/com/teambrmodding/neotech/Neotech.java
@@ -1,28 +1,25 @@
 package com.teambrmodding.neotech;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import com.teambrmodding.neotech.lib.Reference;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
-@Mod("neotech")
+/**
+ * This file was created for AssistedProgression
+ *
+ * AssistedProgression is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * @author James Rogers - Dyonovan
+ * @since ${DATE}
+ */
+@Mod(Reference.MOD_ID)
 public class Neotech
 {
-
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public Neotech() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
---
+++ src/main/java/com/teambrmodding/neotech/lib/Reference.java
@@ -0,0 +1,17 @@
+package com.teambrmodding.neotech.lib;
+
+/**
+ * This file was created for AssistedProgression
+ * <p>
+ * AssistedProgression is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * @author James Rogers - Dyonovan
+ * @since 9/2/2019
+ */
+public class Reference {
+    public static final String MOD_NAME     = "Neotech";
+    public static final String MOD_ID       = "neotech";
+    public static final String VERSION      = "6.0";
+}
