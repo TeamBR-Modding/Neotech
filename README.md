0b776d48ced9762892be2dd7b98e043ed5d9dece
Dyonovan <dyonovan@gmail.com>
2019-09-02 22:59:00 -0400

    Initial Commit for 1.14.4

--- README.md
+++
@@ -1,9 +0,0 @@
-NeoTech <a href='http://teambrmodding.com:8111/viewType.html?buildTypeId=NeoTech_Build&tab=buildTypeStatusDiv&branch_NeoTech=%3Cdefault%3E'><img src="http://teambrmodding.com:8111/app/rest/builds/buildType:(id:NeoTech_Build)/statusIcon"/></a><a href='https://minecraft.curseforge.com/projects/neotech'><img src='http://cf.way2muchnoise.eu/full_neotech_downloads.svg'><img src='http://cf.way2muchnoise.eu/versions/neotech_latest.svg'></a>
-===============
-Tech mod for Forge/Minecraft 1.11.2
-
-License
-===============
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">NeoTech</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Team BR Modding</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/TeamBR-Modding/NeoTech" rel="dct:source">https://github.com/TeamBR-Modding/NeoTech</a>.
-
-
--- src/main/java/com/teambrmodding/neotech/Neotech.java
+++ src/main/java/com/teambrmodding/neotech/Neotech.java
@@ -1,102 +1,33 @@
 package com.teambrmodding.neotech;
 
-import com.teambrmodding.neotech.collections.CreativeTabMetals;
-import com.teambrmodding.neotech.common.CommonProxy;
-import com.teambrmodding.neotech.lib.Reference;
-import com.teambrmodding.neotech.managers.*;
-import com.teambrmodding.neotech.network.PacketDispatcher;
-import com.teambrmodding.neotech.world.OreGeneratorManager;
-import net.minecraft.command.ServerCommandManager;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.File;
+import java.util.stream.Collectors;
 
-/**
- * This file was created for NeoTech
- *
- * NeoTech is licensed under the
- * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
- * http://creativecommons.org/licenses/by-nc-sa/4.0/
- *
- * @author Paul Davis - pauljoda
- * @since 2/15/2017
- */
-@Mod(modid          = Reference.MOD_ID,
-        name           = Reference.MOD_NAME,
-        version        = Reference.VERSION,
-        dependencies   = Reference.DEPENDENCIES,
-        updateJSON     = Reference.UPDATE_JSON,
-        guiFactory     = "com.teambrmodding.neotech.client.ingameconfig.GuiFactoryNeotech")
-public class Neotech {
+@Mod("neotech")
+public class Neotech
+{
 
-    // Enable the bucket on creation
-    static {
-        FluidRegistry.enableUniversalBucket();
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Neotech() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
 
-    @Mod.Instance
-    public static Neotech INSTANCE;
-
-    public static String configFolderLocation;
-
-    @SidedProxy(
-            clientSide = "com.teambrmodding.neotech.client.ClientProxy",
-            serverSide = "com.teambrmodding.neotech.common.CommonProxy"
-    )
-    public static CommonProxy proxy;
-
-    // Our main Creative Tab
-    public static CreativeTabs tabNeotech = new CreativeTabs("tabNeotech") {
-        @Override
-        public ItemStack getTabIconItem() {
-            return new ItemStack(Item.getItemFromBlock(BlockManager.eliteEnergyStorage));
-        }
-    };
-
-    // Our metals tab
-    public static CreativeTabMetals tabMetals = new CreativeTabMetals();
-
-    @Mod.EventHandler
-    public static void preInit(FMLPreInitializationEvent event) {
-        CapabilityLoadManager.registerCapabilities();
-        configFolderLocation = event.getModConfigurationDirectory().getAbsolutePath() + File.separator + "Neotech";
-
-        BlockManager.preInit();
-        ItemManager.preInit();
-        FluidManager.preInit();
-        MetalManager.registerDefaultMetals();
-        RecipeManager.preInit();
-
-        ConfigManager.preInit();
-        GameRegistry.registerWorldGenerator(OreGeneratorManager.INSTANCE, 2);
-
-        proxy.preInit();
-    }
-
-    @Mod.EventHandler
-    public static void init(FMLInitializationEvent event) {
-        RecipeManager.init();
-        PacketDispatcher.initPackets();
-        proxy.init();
-    }
-
-    @Mod.EventHandler
-    public static void posInit(FMLPostInitializationEvent event) {
-        proxy.postInit();
-    }
-
-    @Mod.EventHandler
-    public static void serverLoad(FMLServerStartingEvent event) {
-        RecipeManager.initCommands((ServerCommandManager) event.getServer().getCommandManager());
+    private void setup(final FMLCommonSetupEvent event) {
     }
 }
